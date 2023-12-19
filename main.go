710ce37314c95308ef27940e58ff1b28dec54d1c
Ashwin Kumar Singh <ashwinkumar2309@protonmail.com>
2023-12-19 21:41:32 +0530

    Add initial code for movie CRUD API upto create movie

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	// import of gorilla mux
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	// present in request of mux
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+		}
+		// deleting by appending the previous and next elements
+		// ... is used to append the slice
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	// present in request of mux
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	// we'll get something in request
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	err := json.NewDecoder(r.Body).Decode(&movie)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	movie.ID = strconv.Itoa(rand.Intn(10000000))
+
+	// append the movie in the movies slice
+	movies = append(movies, movie)
+
+	// return the movie
+	json.NewEncoder(w).Encode(movie)
+}
+
+func main() {
+	// no databases so only structs and slices only
+
+	r := mux.NewRouter()
+	// inside gorilla mux
+
+	movies = append(movies, Movie{ID: "1", Isbn: "12345", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "123456", Title: "Movie Two", Director: &Director{Firstname: "Steve", Lastname: "Smith"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	// r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
